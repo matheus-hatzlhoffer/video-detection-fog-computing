f65ef0b3c0010996200ad2423542b1bd0754a52d
PiethroC <93672408+PiethroC@users.noreply.github.com>
2023-10-18 20:55:13 -0300

    added trial

---
+++ trial/client.py
@@ -0,0 +1,28 @@
+import socket, cv2, pickle, struct
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host_ip = "26.91.194.33"
+port = 9998
+client_socket.connect((host_ip, port))
+data = b""
+payload_size=struct.calcsize("Q")
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data+=packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+    while len(data ) < msg_size:
+        data += client_socket.recv(4*1024)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Recieving video from server", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+client_socket.close()
+
