4fff84a9525dba30ec63ade0bffaee300e8c8aec
unknown <VETTORI@BRASIL17.kea-partners.priv>
2023-11-28 00:39:53 -0300

    changing camera-server relationship

--- Yolo/object-detection-opencv/camera.py
+++ Yolo/object-detection-opencv/camera.py
@@ -1,42 +1,42 @@
-import socket, cv2, pickle, struct
-import imutils
+import socket
 import cv2
+import pickle
+import struct
+import imutils
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_name = socket.gethostname()
 host_ip = "127.0.1.1"
-
-print('HOST IP:', host_ip)
-port = 9999
+port = 9997
 socket_address = (host_ip, port)
-server_socket.bind(socket_address)
-server_socket.listen()
-print('Listening at:', socket_address)
 
 def start_video_stream():
-    client_socket, addr = server_socket.accept()
     camera = False
-    if camera == True:
+
+    if camera:
         vid = cv2.VideoCapture(0)
     else:
         vid = cv2.VideoCapture("LondonBikeRideCityTour2022.mp4")
+
     try:
-        print('Client {} CONNECTED!'.format(addr))
-        if client_socket:
-            while(vid.isOpened()):
-                img, frame = vid.read()
+        server_socket.connect(socket_address)
+        print('Connected to server')
 
-                frame = imutils.resize(frame, width=320)
-                a = pickle.dumps(frame)
-                message = struct.pack("Q", len(a))+a
-                client_socket.sendall(message)
-                cv2.imshow("Transmitting to server", frame)
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord('q'):
-                    client_socket.close()
+        while vid.isOpened():
+            img, frame = vid.read()
+
+            frame = imutils.resize(frame, width=320)
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
+            server_socket.sendall(message)
+            cv2.imshow("Transmitting to server", frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+
     except Exception as e:
-        print(f"SERVER {addr} DISCONNECTED")
-        pass
+        print(f"Error: {e}")
+    finally:
+        server_socket.close()
 
-while(True):
-    start_video_stream()
+start_video_stream()
