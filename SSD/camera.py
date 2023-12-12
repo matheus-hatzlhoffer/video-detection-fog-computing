6295fd2c195c5c5a0c661a4833fc7e1a1685d4fd
Piethro Cesar <piethrocesar@gmail.com>
2023-12-11 22:43:30 -0300

    Added video measurements

--- SSD/camera.py
+++ SSD/camera.py
@@ -2,6 +2,7 @@ import socket, cv2, pickle, struct
 import imutils
 import cv2
 import time
+f = open("logfile_camera.txt", "w")
 
 
 def start_video_stream():
@@ -9,11 +10,12 @@ def start_video_stream():
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # host_ip = "127.0.0.1"
-    host_ip = "192.168.0.131"
+    # host_ip = "192.168.0.131"
+    host_ip = "10.3.77.135"
     
     port = 9999
     socket_address = (host_ip, port)
-
+    frame_count = 0
 
 
     camera = False
@@ -26,18 +28,22 @@ def start_video_stream():
         server_socket.connect(socket_address)
         print('Connected to server')
         while(vid.isOpened()):
+            start_time = time.time()
             img, frame = vid.read()
             time.sleep(0.02)
             try:
-                frame = imutils.resize(frame, width=640)
+                frame = imutils.resize(frame, width=240)
             except Exception as ex:
                 print("End of Video")
                 pass
             a = pickle.dumps(frame)
             message = struct.pack("Q", len(a))+a
             server_socket.sendall(message)
-            cv2.imshow("Transmitting to server", frame)
-            key = cv2.waitKey(1) & 0xFF
+            frame_count += 1
+            end_time = time.time()
+            f.write(str("Frame: "+str(frame_count)+" Time: "+str(end_time-start_time)+" bitrate: "+str(len(message)/(end_time-start_time))+" frametime: "+str(time.time())+"\n"))
+            # cv2.imshow("Transmitting to server", frame)
+            # key = cv2.waitKey(1) & 0xFF
             # if key == ord('q'):
         server_socket.close()
     except Exception as e:
--- SSD/client.py
+++ SSD/client.py
@@ -1,4 +1,4 @@
-import socket, cv2, pickle, struct
+import socket, cv2, pickle, struct, time
 
 
 def recieveVideo():
@@ -6,14 +6,19 @@ def recieveVideo():
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # host_ip = "127.0.0.1"
-        host_ip = "192.168.0.131"
+        # host_ip = "192.168.0.131"
+        host_ip = "10.3.77.135"
 
         port = 9997
         client_socket.connect((host_ip, port))
+        frame_count = 0
+
+        f = open("logfile_client.txt", "w")
 
         data = b""
         payload_size=struct.calcsize("Q")
         while True:
+            start_time = time.time()
             while len(data) < payload_size:
                 packet = client_socket.recv(4*1024)
                 if not packet: 
@@ -31,6 +36,9 @@ def recieveVideo():
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 break
+            frame_count += 1
+            end_time = time.time()
+            f.write(str("Frame: "+str(frame_count)+" Time: "+str(end_time-start_time)+" bitrate: "+str(len(data)/(end_time-start_time))+" frametime: "+str(time.time())+"\n"))
         client_socket.close()
     except Exception as e:
         print(e)
