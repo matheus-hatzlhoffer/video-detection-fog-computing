59060073bd530632faf3a1fbf19c0dec7e516e11
PiethroCesar <piethrocesar@gmail.com>
2023-12-13 23:09:31 -0300

    added metrics in yolo

--- Yolo/object-detection-opencv/camera.py
+++ Yolo/object-detection-opencv/camera.py
@@ -1,43 +1,72 @@
-import socket
-import cv2
-import pickle
-import struct
+import socket, cv2, pickle, struct
 import imutils
+import cv2
 import time
+f = open("logfile_camera.csv", "w")
+f.write(str("FPS,Frame,Code,Time,bitrate,frametime\n"))
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = "127.0.0.1"
-port = 9999
-socket_address = (host_ip, port)
+
 
 def start_video_stream():
-    camera = False
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    if camera:
+    # host_ip = "127.0.0.1"
+    # host_ip = "192.168.0.131"
+    host_ip = "10.3.77.135"
+    
+    port = 9999
+    socket_address = (host_ip, port)
+    frame_count = 0
+
+
+    camera = False
+    if camera == True:
         vid = cv2.VideoCapture(0)
     else:
         vid = cv2.VideoCapture("LondonBikeRideCityTour2022.mp4")
-
+        # vid = cv2.VideoCapture("videoTeste.mp4")
     try:
         server_socket.connect(socket_address)
         print('Connected to server')
+        frame_count_fps = 0
+        fps = 0
+        start_time_2 = time.time()    
 
-        while vid.isOpened():
+
+        while(vid.isOpened()):
+            start_time = time.time()
+            frame_count += 1
             img, frame = vid.read()
-            time.sleep(0.02)
-            frame = imutils.resize(frame, width=320)
-            a = pickle.dumps(frame)
-            message = struct.pack("Q", len(a)) + a
-            server_socket.sendall(message)
-            cv2.imshow("Transmitting to server", frame)
-
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
+            # time.sleep(0.02)
+            try:
+                frame = imutils.resize(frame, width=240)
+            except Exception as ex:
+                print("End of Video")
                 break
+                pass
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a))+struct.pack("P", int(str(frame_count).zfill(12)) )+a
+            server_socket.sendall(message)
+            frame_count_fps += 1
+            elapsed_time = time.time() - start_time_2
 
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
+            if elapsed_time > 1:
+                fps = frame_count_fps / elapsed_time
+                print(f"FPS: {fps:.2f}")                    
+                frame_count_fps = 0
+                start_time_2 = time.time()
+
+            end_time = time.time()
+            f.write(str(str(fps)+","+str(frame_count)+","+str(frame_count).zfill(12)+","+str(end_time-start_time)+","+str(len(message)/(end_time-start_time))+","+str(time.time())+"\n"))
+            cv2.imshow("Transmitting to server", frame)
+            key = cv2.waitKey(1) & 0xFF
+            # if key == ord('q'):
         server_socket.close()
+    except Exception as e:
+        print(e)
+        # print(f"COULDN'T CONECT TO SERVER, TRYING AGAIN")
+        pass
 
-start_video_stream()
+while(True):
+    start_video_stream()
--- Yolo/object-detection-opencv/client.py
+++ Yolo/object-detection-opencv/client.py
@@ -1,28 +1,62 @@
-import socket, cv2, pickle, struct
+import socket, cv2, pickle, struct, time
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host_ip = "127.0.0.1"
-port = 9997
-client_socket.connect((host_ip, port))
-data = b""
-payload_size=struct.calcsize("Q")
-while True:
-    while len(data) < payload_size:
-        packet = client_socket.recv(4*1024)
-        if not packet: break
-        data+=packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
-    while len(data ) < msg_size:
-        data += client_socket.recv(4*1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("Recieving video from server", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-client_socket.close()
+def recieveVideo():
 
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # host_ip = "127.0.0.1"
+        # host_ip = "192.168.0.131"
+        host_ip = "10.3.77.135"
+
+        port = 9997
+        client_socket.connect((host_ip, port))
+        frame_count = 0
+        frame_count_fps = 0
+        fps=0
+
+        f = open("logfile_client.csv", "w")
+        f.write(str("FPS,Frame,Code,Time,bitrate,frametime\n"))
+
+        data = b""
+        payload_size=struct.calcsize("Q")
+        code_payload = struct.calcsize("P")
+        start_time_2 = time.time()    
+        while True:
+            start_time = time.time()
+            while len(data) < payload_size+code_payload:
+                packet = client_socket.recv(4*1024)
+                if not packet: 
+                    break
+                data+=packet
+            packed_msg_size = data[:payload_size]
+            code = data[payload_size:payload_size+code_payload]
+            # print(struct.unpack("P", code)[0])
+            data = data[payload_size+code_payload:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data ) < msg_size:
+                data += client_socket.recv(4*1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            cv2.imshow("Recieving video from server", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+            frame_count += 1
+            frame_count_fps += 1
+
+            elapsed_time = time.time() - start_time_2
+            if elapsed_time > 1:
+                fps = frame_count_fps / elapsed_time
+                print(f"FPS: {fps:.2f}")                    
+                frame_count_fps = 0
+                start_time_2 = time.time()
+            end_time = time.time()
+            f.write(str(str(fps)+","+str(frame_count)+","+str(struct.unpack("P", code)[0]).zfill(12)+","+str(end_time-start_time)+","+str((msg_size)/(end_time-start_time))+","+str(time.time())+"\n"))
+        client_socket.close()
+    except Exception as e:
+        print(e)
+
+while(True):
+    recieveVideo()
